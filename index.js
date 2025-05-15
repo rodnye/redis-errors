4303d8b0b46b1a07eca763953dd7101fbeb38cd6
Rodny Estrada <rrodnyestrada1@gmail.com>
2025-05-14 20:33:50 -0400

    feat: add support for Next.js 13+ in Edge Runtime

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 'use strict'
 
-const Errors = process.version.charCodeAt(1) < 55 && process.version.charCodeAt(2) === 46
-  ? require('./lib/old') // Node.js < 7
-  : require('./lib/modern')
+let Errors;
+
+/**
+ * Support Next.js 13+ in Edge Runtime
+ * @see https://github.com/vercel/next.js/issues/73424
+ * @see https://nextjs.org/docs/app/api-reference/edge
+ */ 
+if (!process.env.NEXT_RUNTIME) {
+  Errors = process.version.charCodeAt(1) < 55 && process.version.charCodeAt(2) === 46
+    ? require('./lib/old') // Node.js < 7
+    : require('./lib/modern')
+}
+else {
+  Errors = require('./lib/modern')
+}
 
 module.exports = Errors
